b475aaf88efd1b68f647c532019e457f47803598
Sai Binayak Rout <111452037+SaiBinayakRout@users.noreply.github.com>
2024-10-05 14:36:59 +0530

    Add files via upload
    done

---
+++ script.js
@@ -0,0 +1,35 @@
+var typed = new Typed(".typewriter-text", {
+    strings: ["", "Student", "Data Analyst", "FrontEnd Developer"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true
+});
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('.section');  // Corrected: changed 'section' to 'sections'
+let navLinks = document.querySelectorAll('.header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;  // Corrected: use 'sec.offsetTop'
+        let height = sec.offsetHeight;  // Corrected: use 'sec.offsetHeight'
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
